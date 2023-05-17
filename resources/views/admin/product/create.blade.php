5f7b3dcc5c57eff2afa61e6b834111394a8f8628
André Felipe Vigarani <avlipe44@gmail.com>
2023-05-16 21:13:41 -0300

    style(admin.product.create): ajustes no código

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -56,6 +56,7 @@
         </nav>
     </div>
 </div>
+
 <!-- header section end -->
 <!-- contact section start -->
 <div class="contact_section layout_padding">
@@ -77,81 +78,15 @@
                 </div>
             </div>
 
-
             <button type="submit" class="btn btn-primary" style="text-align: center;">Cadastrar</button>
         </form>
     </div>
 </div>
-<!-- contact section end -->
-<!-- footer section start -->
-<!-- <div class="footer_section layout_padding">
-   <div class="container">
-      <div class="row">
-         <div class="col-lg-3 col-sm-6"> -->
-<!-- <div class="fooer_logo"><img src="images/footer-logo.png"></div>
-<p class="footer_lorem_text">There are many variat
-   ions of passages of L
-   orem Ipsum available
-   , but the majority h
-   ave suffered altera
-   tion in some form, by
-</p>
-</div>
-<div class="col-lg-3 col-sm-6">
-<h1 class="customer_text">LET US HELP YOU</h1>
-<p class="footer_lorem_text">There are many variat
-   ions of passages of L
-   orem Ipsum available
-   , but the majority h
-   ave suffered altera
-   tion in some form, by
-</p>
-</div>
-<div class="col-lg-3 col-sm-6">
-<h1 class="customer_text">INFORMATION</h1>
-<p class="footer_lorem_text1">About Us<br>
-   Careers<br>
-   Sell on shopee<br>
-   Press & News<br>
-   Competitions<br>
-   Terms & Conditions
-</p>
-</div>
-<div class="col-lg-3 col-sm-6">
-<h1 class="customer_text">OUR Design</h1>
-<p class="footer_lorem_text">There are many variat
-   ions of passages of L
-   orem Ipsum available
-   , but the majority h
-   ave suffered altera
-   tion in some form, by
-</p>
-</div>
-</div>
-<div class="input-group mb-3">
-<input type="text" class="form-control" placeholder="Enter your email" aria-label="Enter your email" aria-describedby="basic-addon2">
-<div class="input-group-append">
-<span class="input-group-text" id="basic-addon2"><a href="#">Subscribe</a></span> -->
-</div>
-</div>
-</div>
-</div>
-<!--  footer section end -->
-<!-- copyright section start -->
+
 <div class="copyright_section">
     <div class="container">
-        <!-- <div class="social_icon">
-           <ul>
-              <li><a href="#"><img src="images/fb-icon.png"></a></li>
-              <li><a href="#"><img src="images/twitter-icon.png"></a></li>
-              <li><a href="#"><img src="images/instagram-icon.png"></a></li>
-              <li><a href="#"><img src="images/linkedin-icon.png"></a></li>
-           </ul>
-        </div> -->
         <p class="copyright_text">2023 All Rights Reserved. Design by MN Sistemas</a></p>
     </div>
-    <!-- copyright section end -->
-    <!-- Javascript files-->
     <script src="/js/jquery.min.js"></script>
     <script src="/js/popper.min.js"></script>
     <script src="/js/bootstrap.bundle.min.js"></script>
@@ -163,5 +98,6 @@
     <!-- javascript -->
     <script src="/js/owl.carousel.js"></script>
     <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+</div>
 </body>
 </html>
