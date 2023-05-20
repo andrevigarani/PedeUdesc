6093e35e3c7d6837fec47c5d9474e40526262288
Luiza Nurnberg <nurnbergluiza@gmail.com>
2023-05-19 21:29:44 -0300

    feat(image): nova imagem adicionada

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,21 +3,25 @@
 @section('content')
 <!DOCTYPE html>
 <html lang="en">
-   <head>
-      <!-- basic -->
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <!-- site metas -->
-      <title>Login</title>
 
-    </head>
-    <body style="background-image: url('images/banner-bg.png'); width: 100%; float: left; height: auto; padding-bottom: 80px; background-size: 100%;">
+<head>
+    <!-- basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- site metas -->
+    <title>Login</title>
+
+</head>
+
+<body
+    style="background-image: url('images/banner-bg.png'); width: 100%; float: left; height: auto; padding-bottom: 80px; background-size: 100%;">
 
     <!--<div class="card-header">{{ __('Login') }}</div> -->
+    <img src="images/background.png" style="position:absolute; height: 300px"><br>
     <div class="banner_section layout_padding">
         <div class="container">
-            <div class="col-md-5 offset-md-4 bg-white"><br><br> 
-            <center><img src="images/logo.png"><br></center>
+            <div class="col-md-5 offset-md-4 bg-white"><br><br>
+                <img src="images/logo.png" style="align-items: center;"><br>
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
@@ -26,26 +30,30 @@
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                    name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <label for="password"
+                                class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password"
+                                    class="form-control @error('password') is-invalid @enderror" name="password"
+                                    required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -53,7 +61,8 @@
                         <div class="row mb-3">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                                        {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
@@ -69,9 +78,9 @@
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
                                 @endif
                             </div>
                         </div>
@@ -80,5 +89,5 @@
             </div>
         </div>
     </div>
-</div>
-@endsection
+    </div>
+    @endsection
